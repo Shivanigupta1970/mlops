de82d9f49913ada5352fd5b304575245c662dcbe
Shivani Devesh Gupta <gshivani2002@gmail.com>
2025-02-07 09:52:39 +0530

    first commit

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -14,6 +14,10 @@ import json
 import mlflow
 from urllib.parse import urlparse
 
+
+def eval_metrics(actual, pred):
+
+
 def tarin_and_eveluate(config_path):
     config=read_param(config_path)
     train_data_path=config["split_data"]["train_path"]
@@ -31,9 +35,39 @@ def tarin_and_eveluate(config_path):
     train=pd.read_csv("train_data_path")
     test=pd.read_csv("test_data_path")
     
+    train_y=train[target]
+    test_y=test[target]
 
+    train_x=train.drop(target, axis=1)
+    test_x=test.drop(target, axis=1)
 
+    lr=ElasticNet(alpha=alpha,l1_ration=l1_ration,random_state=random_state)
+    lr.fit(train_x,train_y)
+    predicted_qualities= lr.predict(test_x)
+    
+    (rmse,mae,r2)=eval_metrics(test_y,predicted_qualities)
+    print("Elasticnet model (alpha=%f, l1_ratio=%f) : " % (alpha,l1_ration))
 
+    scores_files=config["reports"]["score"]
+    params_files=config["reports"]["params"]
+
+    with open(scores_files,"w") as f:
+        scores={
+            "rmse":rmse,
+            "mae":mae,
+            "r2":r2
+        }
+        json.dump(scores,f)
+
+    with open(params_files,"w") as f:
+        params={
+            "alpha":alpha,
+            "l1_ration":l1_ration
+        }
+        json.dump(params,f)
+
+model_path=config("model_path")
+joblib.dump(lr, model_path)        
 
 if __name__=="__main__":
     args=argparse.ArgumentParser()
